00e73bb08feec7a8072a23c2f8f4d619cf2b968b
David Vogel <dvogel@fastmail.com>
2021-03-14 15:16:39 -0400

    Initial commit

---
+++ hosts/dvtp/default.nix
@@ -0,0 +1,63 @@
+{ pkgs, config, lib, ... }:
+{
+  imports = [
+    ../home.nix
+    ./hardware-configuration.nix
+  ];
+
+  ## Modules
+  modules = {
+    desktop = {
+      bspwm.enable = true;
+      apps = {
+        discord.enable = true;
+        rofi.enable = true;
+      };
+      browsers = {
+        default = "brave";
+        brave.enable = true;
+        firefox.enable = true;
+        qutebrowser.enable = true;
+      };
+      media = {
+        daw.enable = true;
+        documents.enable = true;
+        graphics.enable = true;
+        mpv.enable = true;
+      };
+      term = {
+        default = "xst";
+        st.enable = true;
+      };
+    };
+    editors = {
+      default = "nvim";
+      emacs.enable = true;
+      vim.enable = true;
+    };
+    shell = {
+      adl.enable = true;
+      bitwarden.enable = true;
+      direnv.enable = true;
+      git.enable    = true;
+      gnupg.enable  = true;
+      zsh.enable    = true;
+    };
+    services = {
+      ssh.enable = true;
+    };
+    theme.active = "alucard";
+  };
+
+
+  ## Local config
+  programs.ssh.startAgent = true;
+  services.openssh.startWhenNeeded = true;
+
+  networking.networkmanager.enable = true;
+  # The global useDHCP flag is deprecated, therefore explicitly set to false
+  # here. Per-interface useDHCP will be mandatory in the future, so this
+  # generated config replicates the default behaviour.
+  networking.useDHCP = false;
+
+}
